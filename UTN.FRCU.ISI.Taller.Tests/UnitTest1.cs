52cc77898cb1c896cb93bdf1f7ba24a6e74328a4
Enzo Tanga <eftanga@gmail.com>
2015-10-07 18:16:12 -0300

    commit inicial

---
+++ UTN.FRCU.ISI.Taller.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UTN.FRCU.ISI.Taller.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
